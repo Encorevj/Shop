e67dfcf48948e37f7b503050498a65fae5ddfe20
encorevj <vaibhavjharbade17@gmail.com>
2021-05-14 12:26:36 +0530

    this is tejendra commit

--- holy/models.py
+++ holy/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 class CATE_GORY(models.Model):
     name=models.CharField(max_length=10)
